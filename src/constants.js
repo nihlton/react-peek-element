60fc0094aeb5c526ca87705ff894c6663e962ce8
greg archer <nihlton@gmail.com>
2020-10-17 10:12:31 -0700

    trim fat

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const MAX_STYLE = { maxWidth: '100%' }
+export const PARENT_STYLE = { position: 'relative', ...MAX_STYLE }
+export const PLACEHOLDER_STYLE = { ...MAX_STYLE }
+export const CHILD_STYLE = {
+  width: '100%',
+  position: 'fixed',
+  zIndex: '4000',
+  ...MAX_STYLE,
+}
+
+export const SCROLLING_DOWN_CLASS = 'scrolling-down'
+export const SCROLLING_UP_CLASS = 'scrolling-up'
--- src/index.js
+++ src/index.js
@@ -1,38 +1,21 @@
 import React, { useEffect, useRef, useCallback } from 'react'
 import ResizeObserver from 'resize-observer-polyfill'
-
-const MAX_STYLE = { maxWidth: '100%' }
-const PARENT_STYLE = { position: 'relative', ...MAX_STYLE }
-const PLACEHOLDER_STYLE = { ...MAX_STYLE }
-const CHILD_STYLE = {
-  width: '100%',
-  position: 'fixed',
-  zIndex: '4000',
-  ...MAX_STYLE,
-}
-
-const SCROLLING_DOWN_CLASS = 'scrolling-down'
-const SCROLLING_UP_CLASS = 'scrolling-up'
+import { PARENT_STYLE, CHILD_STYLE, PLACEHOLDER_STYLE, SCROLLING_DOWN_CLASS, SCROLLING_UP_CLASS } from './constants'
 
 const PeekElement = function (props) {
-  const containerRef = useRef()
-  const childRef = useRef()
-  const placeHolderRef = useRef()
   const { config } = props
-  const {
-    sizeListener = Function.prototype,
-    revealDuration = 0,
-    placeHolderProps = {},
-    parentProps = {},
-    childProps = {},
-  } = config || {}
+  const { sizeListener = Function.prototype, revealDuration = 0, } = config || {}
+  const { placeHolderProps = {}, parentProps = {}, childProps = {}, } = config || {}
 
-  const lastScrollPosition = React.useRef(window.scrollY)
+  const placeHolderRef = useRef(null)
+  const containerRef = useRef(null)
+  const childRef = useRef(null)
   const scrollDelta = React.useRef(0)
   const childHeight = React.useRef(0)
   const childWidth = React.useRef(0)
   const childTop = React.useRef(0)
   const inProcess = React.useRef(false)
+  const lastScrollPosition = React.useRef(window.scrollY)
 
   const handleRepositionAction = useCallback(() => {
     if (inProcess.current) { return }
@@ -43,24 +26,26 @@ const PeekElement = function (props) {
     const parentRect = containerRef.current.getBoundingClientRect()
     const scrollingUp = lastScrollPosition.current > window.scrollY
     const scrollingDown = lastScrollPosition.current < window.scrollY
+    let newClass, oldClass, newChildTop = childTop.current
 
-    let newChildTop = childTop.current
     scrollDelta.current = Math.abs(lastScrollPosition.current - window.scrollY)
 
     if (scrollingUp) {
-      child.classList.add(SCROLLING_UP_CLASS)
-      child.classList.remove(SCROLLING_DOWN_CLASS)
+      newClass = SCROLLING_UP_CLASS
+      oldClass = SCROLLING_DOWN_CLASS
       newChildTop += scrollDelta.current
     } else if (scrollingDown) {
-      child.classList.add(SCROLLING_DOWN_CLASS)
-      child.classList.remove(SCROLLING_UP_CLASS)
+      newClass = SCROLLING_DOWN_CLASS
+      oldClass = SCROLLING_UP_CLASS
       newChildTop -= scrollDelta.current
     }
 
     if (window.scrollY === 0) {
-      child.classList.remove(SCROLLING_UP_CLASS)
+      oldClass = SCROLLING_UP_CLASS
     }
 
+    newClass && !child.classList.contains(newClass) && child.classList.add(newClass)
+    oldClass && child.classList.contains(oldClass) && child.classList.remove(oldClass)
     newChildTop = Math.min(0, Math.max(-childRect.height, newChildTop))
 
     if (newChildTop !== childTop.current) {
@@ -98,10 +83,6 @@ const PeekElement = function (props) {
     }
   }, [containerRef, handleRepositionAction])
 
-  const parentStyle = { ...PARENT_STYLE, ...(parentProps?.style || {}) }
-  const childStyle = { ...CHILD_STYLE, ...(childProps?.style || {}) }
-  const placeHolderStyle = { ...PLACEHOLDER_STYLE, ...(placeHolderProps?.style || {}) }
-
   const animateTo = (to) => {
     const child = childRef?.current
 
@@ -116,17 +97,15 @@ const PeekElement = function (props) {
     }
   }
 
-  const api = {
-    hide: () => animateTo(-childHeight?.current),
-    show: () => animateTo(0),
-  }
-
-  const renderChildren = (children) => typeof children === 'function' ? children(api) : children
+  const parentStyle = { ...PARENT_STYLE, ...(parentProps?.style || {}) }
+  const childStyle = { ...CHILD_STYLE, ...(childProps?.style || {}) }
+  const placeHolderStyle = { ...PLACEHOLDER_STYLE, ...(placeHolderProps?.style || {}) }
+  const api = { show: () => animateTo(0), hide: () => animateTo(-childHeight?.current), }
 
   return (
     <div ref={containerRef} {...parentProps} style={parentStyle}>
       <div ref={childRef} {...childProps} style={childStyle}>
-        {renderChildren(props.children)}
+        { typeof props.children === 'function' ? props.children(api) : props.children }
       </div>
       <div ref={placeHolderRef} {...placeHolderProps} style={placeHolderStyle} />
     </div>
